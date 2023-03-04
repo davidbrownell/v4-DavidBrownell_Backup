6ac81d81db61cad21bd318dfe9db5e5b07c777f7
David Brownell <db@DavidBrownell.com>
2023-03-04 10:20:23 -0800

    👽️ [external_api_change] AutoSemVer interface (Change 1 of 2)

--- Scripts/Backup/setup.py
+++ Scripts/Backup/setup.py
@@ -46,7 +46,7 @@ with ExitStack(lambda: sys.path.pop(0)):
 # ----------------------------------------------------------------------
 def _GetVersion() -> str:
     result = SubprocessEx.Run(
-        'AutoSemVer{ext} Generate --no-metadata --style AllMetadata --quiet'.format(
+        'AutoSemVerTemp{ext} --no-metadata --style AllMetadata --quiet'.format(
             ext=CurrentShell.script_extensions[0],
         ),
         cwd=_this_dir,
